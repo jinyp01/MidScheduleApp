29a3599242f6fb6026fb6975160d1592127bcfc7
jy030506@naver.com <jy030506@naver.com>
2026-04-22 12:00:49 +0900

    feat : "댓글 검색 기능 추가"
    일정에 달린 모든 댓글을 검색

--- src/main/java/com/example/midscheduleapp/comment/Controller/CommentController.java
+++ src/main/java/com/example/midscheduleapp/comment/Controller/CommentController.java
@@ -3,14 +3,14 @@ package com.example.midscheduleapp.comment.Controller;
 import com.example.midscheduleapp.BaseEntity;
 import com.example.midscheduleapp.comment.Dto.CreateCommentRequest;
 import com.example.midscheduleapp.comment.Dto.CreateCommentResponse;
+import com.example.midscheduleapp.comment.Dto.GetCommentResponse;
 import com.example.midscheduleapp.comment.Service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +26,13 @@ public class CommentController {
             ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 commentService.create(userId, scheduleId, request));
+    }
+
+    @GetMapping("/users/{userId}/schedules/{scheduleId}/comments")
+    ResponseEntity<List<GetCommentResponse>> getAllComment(
+            @PathVariable Long scheduleId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.getAll(scheduleId));
     }
 
 
--- src/main/java/com/example/midscheduleapp/comment/Service/CommentService.java
+++ src/main/java/com/example/midscheduleapp/comment/Service/CommentService.java
@@ -2,6 +2,7 @@ package com.example.midscheduleapp.comment.Service;
 
 import com.example.midscheduleapp.comment.Dto.CreateCommentRequest;
 import com.example.midscheduleapp.comment.Dto.CreateCommentResponse;
+import com.example.midscheduleapp.comment.Dto.GetCommentResponse;
 import com.example.midscheduleapp.comment.Entity.Comment;
 import com.example.midscheduleapp.comment.Repository.CommentRepository;
 import com.example.midscheduleapp.schedule.Entity.Schedule;
@@ -11,6 +12,9 @@ import com.example.midscheduleapp.user.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -38,5 +42,28 @@ public class CommentService {
                 comment.getCreatedAt(),
                 comment.getUpdatedAt()
         );
+    }
+
+    // 일정에 속한 모든 댓글 검색
+    @Transactional(readOnly = true)
+    public List<GetCommentResponse> getAll(Long scheduleId) {
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new IllegalStateException("해당하는 일정이 없습니다.")
+        );
+
+        // 해당 일정에 속한 모든 댓글들을 List로 만들어 반환
+        List<GetCommentResponse> dtos =
+                commentRepository.findBySchedule(schedule)
+                        .stream()
+                        .map(comment -> new GetCommentResponse(
+                                comment.getId(),
+                                comment.getContent(),
+                                comment.getCreatedAt(),
+                                comment.getUpdatedAt()))
+                        .toList();
+
+        return dtos;
+
+
     }
 }
